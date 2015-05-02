2cd90add89eb8713034ebaf3a9e88138edd69250
ruraj.joshi@rts.com.np <ruraj.joshi@rts.com.np>
2015-05-02 12:23:03 +0545

    Initial Files added

---
+++ src/com/ruraj/volunteering/StartVolunteering.java
@@ -0,0 +1,20 @@
+package com.ruraj.volunteering;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class StartVolunteering extends Activity {
+  /**
+   * Called when the activity is first created.
+   */
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+
+    WebView webView = (WebView) findViewById(R.id.webView);
+
+    webView.loadUrl("www.ruraj.com/volunteer/app/main");
+  }
+}
