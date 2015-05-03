8d8f952f61e763d2b8d6c8abbfd358268e9a447a
Kaushik Kasaju <kasaju97@gmail.com>
2015-05-03 19:12:41 +0545

    Implementation in MVC (for now due to errors in linux), tested some models

---
+++ server/volunteering/application/controllers/Admin_post.php
@@ -0,0 +1,31 @@
+<?php
+
+	class Admin_post extends CI_CONTROLLER{
+		function index(){
+			$this->load->view('Post_view');
+		}
+
+		function add_detail(){
+
+			$this->load->model('add_post');
+
+			$this->load->library('form_validation');
+			$this->form_validation->set_rules($this->add_post->config);
+
+			if($this->form_validation->run()==TRUE){
+				$this->add_post->add_detail();
+			}else{
+				$this->index();
+			}
+		}
+
+		function get_detail(){
+
+			$this->load->model('get_posts');
+			$this->get_posts->get_detail();
+		}
+
+		
+	}
+
+?>
