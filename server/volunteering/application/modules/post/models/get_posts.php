78cf42e36a7f5d2affb0206df864d423ab1253af
Kaushik Kasaju <kasaju97@gnail.com>
2015-05-13 20:21:39 +0545

    Set conditions for limit and offsets only if query provided

--- server/volunteering/application/modules/post/models/get_posts.php
+++ server/volunteering/application/modules/post/models/get_posts.php
@@ -9,8 +9,7 @@ class Get_posts extends CI_MODEL
 
     function get_posts()
     {
-        $from=$this->input->get('from');
-        $count=$this->input->get('count');
+
         $this->db->select('*');
         $this->db->from('users');
         $this->db->join('post', 'post.user_id=user.id');
@@ -18,7 +17,11 @@ class Get_posts extends CI_MODEL
         $this->db->join('location', 'location.id=post_location.location_id');
         $this->db->join('post_category', 'post_category.post_id=post.id');
         $this->db->join('category', 'category.id=post_category.category_id');
-        $this->db->limit($count,$from);
+        if(isset($_GET['from']) && isset($_GET['count'])){
+            $from=$this->input->get('from');
+            $count=$this->input->get('count');
+            $this->db->limit($count,$from);
+        }
         $q = $this->db->get();
         if ($q->num_rows() > 0) {
             $arr = $q->result();
